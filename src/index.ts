0d340dcf91051c713bfc02ada2f422c92adf3df5
maherhms <maher.3000@outlook.fr>
2026-03-24 12:53:31 +0100

    Implement GET subject API Route

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express, { Request, Response } from 'express';
+import subjectsRouter from "./routes/subjects";
+import cors from "cors"
 
 const app = express();
 const port = 8000;
 
+app.use(cors({
+  origin : process.env.FRONTEND_URL,
+  methods : ["GET" , "POST" , "PUT" , "DELETE"],
+  credentials : true,
+}))
+
 // JSON middleware
 app.use(express.json());
+
+app.use("/api/subjects" , subjectsRouter)
 
 // Root GET route
 app.get('/', (req: Request, res: Response) => {
