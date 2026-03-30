7528645b030051a460d92a5e87b587c63821265e
maherhms <maher.3000@outlook.fr>
2026-03-30 13:52:40 +0200

    Fix authentication issues

--- src/index.ts
+++ src/index.ts
@@ -16,13 +16,16 @@ app.use(cors({
   credentials : true,
 }))
 
-app.all('/api/auth/*splat', toNodeHandler(auth));
-
 // JSON middleware
 app.use(express.json());
 
+// Better Auth
+app.all('/api/auth/*splat', toNodeHandler(auth));
+
+// Arcjet Security Middleware
 app.use(securityMiddleware);
 
+// Routes
 app.use("/api/subjects" , subjectsRouter)
 
 // Root GET route
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,9 +3,16 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../db"; // your drizzle instance
 import * as schema from "../db/schema/auth";
 
+if (!process.env.BETTER_AUTH_SECRET) {
+        throw new Error("BETTER_AUTH_SECRET is not defined");
+    }
+if (!process.env.FRONTEND_URL) {
+    throw new Error("FRONTEND_URL is not defined");
+}
+
 export const auth = betterAuth({
-    secret: process.env.BETTER_AUTH_SECRET!,
-    trustedOrigins: [process.env.FRONTEND_URL!],
+    secret: process.env.BETTER_AUTH_SECRET,
+    trustedOrigins: [process.env.FRONTEND_URL],
     database: drizzleAdapter(db, {
         provider: "pg",
         schema,
