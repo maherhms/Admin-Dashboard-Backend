2d7909e2202537a4e8f3f14aafbf07176d1185fc
maherhms <maher.3000@outlook.fr>
2026-03-30 14:21:25 +0200

    add file extensions

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "../db"; // your drizzle instance
-import * as schema from "../db/schema/auth";
+import { db } from "../db/index.js"; // your drizzle instance
+import * as schema from "../db/schema/auth.js";
 
 if (!process.env.BETTER_AUTH_SECRET) {
         throw new Error("BETTER_AUTH_SECRET is not defined");
