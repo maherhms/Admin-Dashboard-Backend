43351d98ce4023dd2a5f10aa83ee0dad4f804df9
maherhms <maher.3000@outlook.fr>
2026-03-30 13:37:20 +0200

    Implement authentication

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import subjectsRouter from "./routes/subjects";
 import cors from "cors"
 import securityMiddleware from "./middleware/security";
+import {toNodeHandler} from "better-auth/node";
+import {auth} from "./lib/auth";
 
 const app = express();
 const port = 8000;
@@ -13,6 +15,8 @@ app.use(cors({
   methods : ["GET" , "POST" , "PUT" , "DELETE"],
   credentials : true,
 }))
+
+app.all('/api/auth/*splat', toNodeHandler(auth));
 
 // JSON middleware
 app.use(express.json());
