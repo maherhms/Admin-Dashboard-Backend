fbbdbf4177cd785df0cf9f3648a2f7734a1dc850
maherhms <maher.3000@outlook.fr>
2026-04-02 17:06:07 +0200

    Implement classes GET router

--- src/middleware/security.ts
+++ src/middleware/security.ts
@@ -13,16 +13,16 @@ const securityMiddleware = async (req : Request , res : Response , next : NextFu
 
         switch (role) {
             case "admin":
-                limit = 20;
+                limit = 60;
                 message = "Admin request limit exceeded (20 per minute). Slow down!";
                 break;
             case "teacher":
             case "student":
-                limit = 10;
+                limit = 30;
                 message = "User request limit exceeded (10 per minute). Please wait.";
                 break;
             default:
-                limit = 5;
+                limit = 20;
                 message = "Guest request limit exceeded (5 per minute). Please Sign up / log in for higher limits.";
                 break;
         }
